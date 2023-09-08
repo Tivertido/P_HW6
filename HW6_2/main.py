3ac0d52a574ba86aba56b592f31eaf5af1bb0729
Tema <Tema>
2023-09-08 16:56:46 +0300

    "

--- HW6_2/main.py
+++ HW6_2/main.py
@@ -10,3 +10,17 @@ def find_indexes(lst, min_val, max_val):
         if min_val <= lst[i] <= max_val:
             indexes.append(i)
     return indexes
+
+# пример использования функции
+my_list = [1, 3, 5, 7, 9, 2, 4, 6, 8, 10]
+min_value = 3
+max_value = 7
+result = find_indexes(my_list, min_value, max_value)
+print(result)  # выводит [1, 2, 3, 6] 
+ 
+# пример использования функции с другим списком
+new_list = [12, 34, 56, 78, 90, 23, 45, 67, 89, 10]
+min_value = 30
+max_value = 70
+result = find_indexes(new_list, min_value, max_value)
+print(result)  # выводит [1, 2, 6, 7]
